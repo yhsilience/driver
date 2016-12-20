4a75ba9a38ff0674441088b381b38769f0a1d463
yhsilience <yanghen361@hotmail.com>
2016-12-20 21:33:24 +0800

    add code

--- day7/memdev.h
+++ day7/memdev.h
@@ -2,22 +2,19 @@
 #define _MEMDEV_H_
 
 #ifndef MEMDEV_MAJOR
-#define MEMDEV_MAJOR 251 /*主设备号*/
+#define MEMDEV_MAJOR 251
 #endif
 
 #ifndef MEMDEV_SIZE
-#define MEMDEV_SIZE 4096　
+#define MEMDEV_SIZE 4096
 #endif
 
 #ifndef MEMDEV_DEVS
-#define MEMDEV_DEVS 2　/*设备数*/
+#define MEMDEV_DEVS 2
 #endif
-
-/*mem设备描述结构体*/
 struct mem_dev
 {
         char *data;
         unsigned long size;
 };
-
 #endif
